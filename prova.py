7cba0cdf398476182799a2a156775d0216aff450
leonardorz5 <leonardo.limal15@hotmail.com>
2022-01-17 16:46:27 -0300

    codigo de estudo

---
+++ prova.py
@@ -0,0 +1,59 @@
+
+#listasalario = []
+
+#for i in range(0,20):
+    #listasalario.append(randint(0, 1500))
+    
+
+#aumento20 = []
+
+#for i in range (len(listasalario[i])):
+    #if (listasalario[i] <= 280):
+        #aumento20.append(listasalario[i])
+
+#print(aumento20)
+from random import randint
+
+
+
+
+
+salarios = []
+
+for i in range(0,40):#para i no alcance
+    salarios.append(randint(100,1500))
+    i+=1
+
+salario20 = []
+a=[salario20]
+b=[]
+v1 = 0.2 #20%
+
+for i in range(len(salarios)):#para i no alcance
+    if (salarios[i] <=280):
+        salario20.append(salarios[i]*0.8)
+
+
+
+        
+        
+
+
+
+
+    
+        
+
+print(salario20)
+print(len(salario20))
+
+
+        
+       
+
+    
+         
+
+
+
+
